c1814ce0cde6d7b2b444ef6eb68a1f5a03a06091
VIEIRA-Michel <michelvieira.pro@gmail.com>
2023-01-10 23:59:42 +0100

    technology page tablet version finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,23 +10,18 @@ const Header = () => {
 
     return (
         <header className="">
-            <div className='flex flex-row items-center relative justify-between h-32 pt-9'>
-                <div className='flex flex-col items-center justify-center ml-10 w-auto'>
-                    <div className="">
+            <div className='h-full md:h-32 flex flex-row items-center relative justify-between  md:pb-9 xl:pb-0 xl:pt-9 '>
+                <div className='w-2/5 flex flex-col items-center justify-center ml-10'>
+                    <div className="w-full">
                         <img src={logo} alt="" />
                     </div>
-                    {/* <div className="">
-                    <h1>Beyond the sky</h1>
-                </div> */}
                 </div>
-                {/* <div className='flex flex-col absolute bg-slate-700 w-2/5 h-px left-36 z-20'>
-            </div> */}
-                <nav className='flex justify-center items-center h-full w-3/5' style={backgroundNavbar}>
+                <nav className='hidden sm:flex justify-center items-center h-full w-3/5' style={backgroundNavbar}>
                     <ul className='w-10/12 h-full flex flex-row justify-center items-center z-10'>
-                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='pr-3 font-medium'>00</span><span className='font-thin tracking-widest'>HOME</span></a></li>
-                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='pr-3 font-medium'>01</span><span className='font-thin tracking-widest'>DESTINATION</span></a></li>
-                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='pr-3 font-medium'>02</span><span className='font-thin tracking-widest'>CREW</span></a></li>
-                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='pr-3 font-medium'>03</span><span className='font-thin tracking-widest'>TECHNOLOGY</span></a></li>
+                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='hidden lg:block pr-3 font-medium'>00</span><span className='font-thin tracking-widest'>HOME</span></a></li>
+                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='hidden lg:block pr-3 font-medium'>01</span><span className='font-thin tracking-widest'>DESTINATION</span></a></li>
+                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='hidden lg:block pr-3 font-medium'>02</span><span className='font-thin tracking-widest'>CREW</span></a></li>
+                        <li className='w-1/4 h-full text-slate-50 flex justify-center items-center'><a href="#home" className='h-full flex items-center text-xl cursor-pointer border-b-2 border-transparent focus:border-white'><span className='hidden lg:block pr-3 font-medium'>03</span><span className='font-thin tracking-widest'>TECHNOLOGY</span></a></li>
                     </ul>
                 </nav>
             </div>
--- src/views/Technology/Technology.js
+++ src/views/Technology/Technology.js
@@ -1,41 +1,43 @@
 import { useState } from "react";
 import data from '../../data/data.json';
-import launchPicture from '../../assets/technology/image-launch-vehicle-portrait.jpg';
-import spaceportPicture from '../../assets/technology/image-spaceport-portrait.jpg';
-import spacePicture from '../../assets/technology/image-space-capsule-portrait.jpg';
+// import launchPicture from '../../assets/technology/image-launch-vehicle-portrait.jpg';
+
+// import spaceportPicture from '../../assets/technology/image-spaceport-portrait.jpg';
+// import spacePicture from '../../assets/technology/image-space-capsule-portrait.jpg';
 
 const Technology = () => {
     const [launch, spaceport, space] = data.technology;
     const [currentSelection, setCurrentSelection] = useState(launch);
 
     return (
-        <main className='w-4/5 flex flex-col justify-center m-auto'>
-            <div className='h-1/4 flex flex-row items-center'>
-                <div>
-                    <span className="text-2xl text-slate-600 tracking-widest">03</span>
-                </div>
-                <div className="ml-4 flex justify-start">
-                    <h1 className="text-2xl text-white font-light tracking-widest">SPACE LAUNCH 101</h1>
+        <main className='w-full flex flex-col justify-center xl:m-0'>
+            <div className='h-1/12 md:h-1/6 md:w-2/5 xl:w-4/6 xl:h-1/4 flex flex-row items-center justify-center xl:justify-start'>
+                <div className="md:w-full xl:w-4/5 flex flex-row items-center md:ml-10 xl:ml-auto">
+                    <div>
+                        <span className="text-2xl text-slate-600 tracking-widest">03</span>
+                    </div>
+                    <div className="ml-4 flex justify-start">
+                        <h1 className="text-2xl text-white font-light tracking-widest">SPACE LAUNCH 101</h1>
+                    </div>
                 </div>
             </div>
-            <div className='h-3/4 flex flew-row'>
-                <div className='w-4/6 flex flex-col justify-evenly'>
-
-                    <div className="h-3/5 flex flex-row">
-                        <div className="w-1/6 flex flex-col justify-between">
-                            <div onClick={() => setCurrentSelection(launch)} className={`h-20 w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Launch vehicle" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">1</span></div>
-                            <div onClick={() => setCurrentSelection(spaceport)} className={`h-20 w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Spaceport" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">2</span></div>
-                            <div onClick={() => setCurrentSelection(space)} className={`h-20 w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Space capsule" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">3</span></div>
+            <div className='h-11/12 md:h-5/6 xl:h-3/4 flex flex-col-reverse xl:flex-row xl:items-center'>
+                <div className='w-full h-2/3 xl:w-4/6 xl:h-full flex flex-col xl:justify-evenly'>
+                    <div className="h-full xl:h-3/5 xl:w-4/5 xl:ml-auto flex flex-col xl:flex-row">
+                        <div className="w-9/12 md:w-2/5 m-auto xl:m-0 h-1/4 xl:h-auto xl:w-1/6 flex flex-row items-center xl:items-start xl:flex-col justify-evenly xl:justify-evenly">
+                            <div onClick={() => setCurrentSelection(launch)} className={`h-16 w-16 xl:h-20 xl:w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Launch vehicle" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">1</span></div>
+                            <div onClick={() => setCurrentSelection(spaceport)} className={`h-16 w-16 xl:h-20 xl:w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Spaceport" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">2</span></div>
+                            <div onClick={() => setCurrentSelection(space)} className={`h-16 w-16 xl:h-20 xl:w-20 rounded-full flex justify-center items-center cursor-pointer ${currentSelection.name === "Space capsule" ? "bg-white" : "bg-transparent text-white border border-slate-600 "}`}><span className="space text-3xl">3</span></div>
                         </div>
-                        <div className="w-5/6 flex flex-col justify-between">
-                            <div className="text-white text-xl font-thin tracking-widest">THE TERMINOLOGY</div>
-                            <div className="text-white text-5xl space">LAUNCH VEHICLE</div>
-                            <div className="text-white text-xl font-thin w-1/2">{currentSelection.description}</div>
+                        <div className="w-full h-3/4 xl:h-auto xl:w-5/6 flex flex-col justify-evenly">
+                            <div className="text-center xl:text-left text-white text-xl font-thin tracking-widest">THE TERMINOLOGY</div>
+                            <div className="text-center xl:text-left text-white text-3xl md:text-5xl space">LAUNCH VEHICLE</div>
+                            <div className="custom-width xl:w-1/2 text-center xl:text-left text-white text-xl font-thin mx-auto xl:m-0">{currentSelection.description}</div>
                         </div>
                     </div>
                 </div>
-                <div className='w-2/6 flex justify-center items-center'>
-                    <img src={currentSelection.name === "Launch vehicle" ? launchPicture : currentSelection.name === "Spaceport" ? spaceportPicture : spacePicture} className="absolute right-0" alt="" />
+                <div className='w-full xl:h-3/5 max-[413px]:h-3/12 h-1/3 xl:w-2/6 xl:h-full flex justify-center items-center'>
+                    <img className={`w-full technology-picture bg-center ${currentSelection.name === "Launch vehicle" ? "launchPicture" : currentSelection.name === "Spaceport" ? "spaceportPicture" : "spacePicture"}`} alt="" />
                 </div>
             </div>
         </main>
